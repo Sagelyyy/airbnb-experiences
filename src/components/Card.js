de2bfc2bbdcf4658b6922e85aa40ccd04080e1dc
Chris <orj.tsuk@gmail.com>
2022-01-19 12:08:50 -0500

    styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,14 +10,11 @@ export default function Card() {
                 <section className="card--details">
                     <img className="card--star" src={star} alt="rating" /> 
                     <span className="card--rating">5.0</span>
-                    <span className="card--rating--count">(6) - </span>
-                    <span className="card--rating--count">USA</span>
+                    <span className="card--rating--count">(6) • </span>
+                    <span className="card--rating--count"> USA </span>
                 </section>
                 <h5 className="card--subtext">Life lessons with Katie Zaferes</h5>
-                <section className="card--price--details">
-                    <h5 className="card--price">From $136 </h5>
-                    <h5 className="card--price--person"> / person</h5>
-                </section>
+                <p className="card--price">From $136 <span className="card--price--person"> / person</span> </p>
        </section>
     )
 }
